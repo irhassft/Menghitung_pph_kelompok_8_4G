f62331a00fe1d71f76485daf0b1b5d1763d8193d
Hafish300 <134570357+Hafish300@users.noreply.github.com>
2023-06-06 13:34:34 +0000

    Add files via upload

---
+++ Hafish_hisyam_300.java
@@ -0,0 +1,6 @@
+public class Hafishhisyam_300_Individu {
+    public static void main(String[] args) {
+        System.out.println("Hafish hisyam ");
+        System.out.println("2113010300");
+    }
+}
