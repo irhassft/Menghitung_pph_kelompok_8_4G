2be178a0963d5ff44519639be91fd62dd1933309
nugroho adi putro <133626074+nugrohoadip@users.noreply.github.com>
2023-06-06 14:23:05 +0700

    Add files via upload

---
+++ adi.java
@@ -0,0 +1,18 @@
+
+public class adi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Nugroho Adi Putro";
+        String nim = "2113010274";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+    }
+}
+
+
+
