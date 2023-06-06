55d9967333f095e78df023a129ee2ed933485832
Irhas Safaat <133625673+irhassft@users.noreply.github.com>
2023-06-06 14:23:01 +0700

    Add files via upload

---
+++ Irhassft.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package irhassft;
+
+/**
+ *
+ * @author yahya
+ */
+public class Irhassft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Irhas Safaat";
+        String nim = "2113010273";
+        String kelas = "4G";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("kelas: " + kelas);
+    }
+    
+}
