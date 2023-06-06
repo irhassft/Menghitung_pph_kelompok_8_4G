560f85aa3cbc1647133fe9ec796652c86b44078f
Indrabk12 <134570008+Indrabk12@users.noreply.github.com>
2023-06-06 15:19:42 +0700

    Add files via upload

---
+++ indrabk_266.java
@@ -0,0 +1,6 @@
+public class ludvijf_282_Individu {
+    public static void main(String[] args) {
+        System.out.println("indra budi kurniawan");
+        System.out.println("2113010266");
+    }
+}
