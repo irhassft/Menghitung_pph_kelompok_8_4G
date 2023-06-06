9954e8822e6dee9e799076c058fe48df819fba8f
ludvijf <133625472+ludvijf@users.noreply.github.com>
2023-06-06 13:44:02 +0700

    Add files via upload

---
+++ ludvijf_java_282.java
@@ -0,0 +1,6 @@
+public class ludvijf_282_Individu {
+    public static void main(String[] args) {
+        System.out.println("ludvi jati fadhilla");
+        System.out.println("2113010282");
+    }
+}
