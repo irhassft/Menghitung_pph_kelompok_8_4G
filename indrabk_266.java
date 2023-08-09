498fb5e4e0a3ad0dba2b48731a1f9584b7ab57db
nugroho adi putro <133626074+nugrohoadip@users.noreply.github.com>
2023-08-09 11:16:38 +0700

    Update indrabk_266.java

--- indrabk_266.java
+++ indrabk_266.java
@@ -1,4 +1,4 @@
-public class ludvijf_282_Individu {
+public class indrabk_266 {
     public static void main(String[] args) {
         System.out.println("indra budi kurniawan");
         System.out.println("2113010266");
